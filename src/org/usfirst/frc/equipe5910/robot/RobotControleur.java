22770531458a94d60883292c81e66c979bd8a2f4
nadineducegep <nadineducegep@gmail.com>
2018-01-30 18:55:50 -0500

    Machoire qui ouvre et ferme selon le mode

--- src/org/usfirst/frc/equipe5910/robot/RobotControleur.java
+++ src/org/usfirst/frc/equipe5910/robot/RobotControleur.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.equipe5910.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Servo;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -11,6 +12,17 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  */
 public class RobotControleur extends IterativeRobot {
 
+	public static final int MACHOIRE_GAUCHE_SERVO = 1; // Servo 1
+	public static final int MACHOIRE_DROITE_SERVO = 0; // Servo 2
+	
+	public static final double MACHOIRE_GAUCHE_FERMEE = 0.0;
+	public static final double MACHOIRE_DROITE_FERMEE = 0.0;
+	public static final double MACHOIRE_GAUCHE_OUVERTE = 1.0;
+	public static final double MACHOIRE_DROITE_OUVERTE = 1.0;
+	
+	Servo verrouMachoireGauche;
+	Servo verrouMachoireDroite;
+	
 	/**
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
@@ -19,6 +31,8 @@ public class RobotControleur extends IterativeRobot {
 	@Override
 	public void robotInit() {
 		System.out.println("robotInit()");
+		verrouMachoireGauche = new Servo(MACHOIRE_GAUCHE_SERVO);
+		verrouMachoireDroite = new Servo(MACHOIRE_DROITE_SERVO);
 	}
 
 	/**
@@ -27,6 +41,9 @@ public class RobotControleur extends IterativeRobot {
 	@Override
 	public void autonomousInit() {
 		System.out.println("autonomousInit()");
+		// Ouvrir la machoire en mode autonome
+		verrouMachoireGauche.set(MACHOIRE_GAUCHE_OUVERTE);
+		verrouMachoireDroite.set(MACHOIRE_DROITE_OUVERTE);
 	}
 
 	/**
@@ -44,6 +61,9 @@ public class RobotControleur extends IterativeRobot {
 	@Override
 	public void teleopInit() {
 		System.out.println("teleopInit()");
+		// Fermer la machoire en mode teleop
+		verrouMachoireGauche.set(MACHOIRE_GAUCHE_FERMEE);
+		verrouMachoireDroite.set(MACHOIRE_DROITE_FERMEE);
 	}
 
 	/**
